cc64d1c20e702c705a657bf1d7fce9d56f7891c3
huongnguyen2152360 <huongnguyen2152360@gmail.com>
2018-07-15 16:56:17 +0700

    huong xinh gai

--- memorycards/js.js
+++
@@ -1,6 +0,0 @@
-var frontCard = $('.front');
-var backCard = $('.back');
-$('.card').click(function() {
-    $('.front').css('transform','rotateY(0deg)');
-    $('.back').css('transform','rotateY(180deg)');
-})
---
+++ memorycards1/name.js
@@ -0,0 +1,58 @@
+const imgs = [
+  "front1.jpg",
+  "front2.jpg",
+  "front3.jpg",
+  "front4.jpg",
+  "front5.jpg",
+  "front6.jpg",
+  "front7.jpg",
+  "front8.jpg",
+  "front9.jpg",
+  "front10.jpg",
+  "front11.jpg",
+  "front12.jpg",
+  "front1.jpg",
+  "front2.jpg",
+  "front3.jpg",
+  "front4.jpg",
+  "front5.jpg",
+  "front6.jpg",
+  "front7.jpg",
+  "front8.jpg",
+  "front9.jpg",
+  "front10.jpg",
+  "front11.jpg",
+  "front12.jpg"
+];
+function loadCard() {
+for (let i = 0; i < imgs.length; i++) {
+  $(".cards-container-row").append(
+    ` <div class="card">
+          <div class="front">
+          <img src="${imgs[i]}">
+          </div>
+          <div class="back">
+          <img src="back2.jpg">
+          </div>
+          </div>`
+  )
+}
+};
+function shuffle(array) {
+  return array.sort(() => {
+    return 0.5 - Math.random();
+  });
+}
+function clickCard() {
+    $(".card").click(function() {
+        // console.log($(this));
+        $(this)[0].children[0].style.transform = "rotateY(0deg)"
+        $(this)[0].children[1].style.transform = "rotateY(180deg)"
+    }
+)
+}
+$(function() {
+shuffle(imgs);
+loadCard();
+clickCard();
+})
