df01bf7a4ead97c5ec5b3789f50a08e0908db26a
huongnguyen2152360 <huongnguyen2152360@gmail.com>
2018-07-16 10:37:13 +0700

    push

--- memorycards2/name.js
+++ memorycards2/name.js
@@ -1,35 +1,35 @@
 const imgs = [
-  "front1.jpg",
-  "front2.jpg",
-  "front3.jpg",
-  "front4.jpg",
-  "front5.jpg",
-  "front6.jpg",
-  "front7.jpg",
-  "front8.jpg",
-  "front9.jpg",
-  "front10.jpg",
-  "front11.jpg",
-  "front12.jpg",
-  "front1.jpg",
-  "front2.jpg",
-  "front3.jpg",
-  "front4.jpg",
-  "front5.jpg",
-  "front6.jpg",
-  "front7.jpg",
-  "front8.jpg",
-  "front9.jpg",
-  "front10.jpg",
-  "front11.jpg",
-  "front12.jpg"
+  "front1",
+  "front2",
+  "front3",
+  "front4",
+  "front5",
+  "front6",
+  "front7",
+  "front8",
+  "front9",
+  "front10",
+  "front11",
+  "front12",
+  "front1",
+  "front2",
+  "front3",
+  "front4",
+  "front5",
+  "front6",
+  "front7",
+  "front8",
+  "front9",
+  "front10",
+  "front11",
+  "front12"
 ];
 function loadCard() {
 for (let i = 0; i < imgs.length; i++) {
   $(".cards-container-row").append(
-    ` <div class="card">
+    ` <div class="card" data-name="${imgs[i]}">
           <div class="front">
-          <img src="${imgs[i]}">
+          <img src="${imgs[i]}.jpg">
           </div>
           <div class="back">
           <img src="back2.jpg">
@@ -43,11 +43,26 @@ function shuffle(array) {
     return 0.5 - Math.random();
   });
 }
+var current = null;
 function clickCard() {
     $(".card").click(function() {
-        console.log($(this));
+        // console.log($(this));
         $(this)[0].children[0].style.transform = "rotateY(0deg)"
         $(this)[0].children[1].style.transform = "rotateY(180deg)"
+        if (!current) { //check if current = null // !null == true
+          current = $(this);
+        }
+        else {
+          if (current.attr('data-name') != $(this).attr('data-name')) {
+            $(this)[0].children[0].style.transform = "rotateY(180deg)"
+            $(this)[0].children[1].style.transform = "rotateY(0deg)"
+            console.log('Different!');
+          }
+          else {
+            $(this)[0].css('display','none');
+            console.log('The same~')
+          }
+        }
     }
 )
 }
