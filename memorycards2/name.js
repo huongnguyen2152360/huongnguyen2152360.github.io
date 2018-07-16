865df314c0a841e4b9c0a16aa00cb0a661c4f811
huongnguyen2152360 <huongnguyen2152360@gmail.com>
2018-07-16 16:33:41 +0700

    push

--- memorycards2/name.js
+++ memorycards2/name.js
@@ -25,9 +25,9 @@ const imgs = [
   "front12"
 ];
 function loadCard() {
-for (let i = 0; i < imgs.length; i++) {
-  $(".cards-container-row").append(
-    ` <div class="card" data-name="${imgs[i]}">
+  for (let i = 0; i < imgs.length; i++) {
+    $(".cards-container-row").append(
+      ` <div class="card" data-name="${imgs[i]}">
           <div class="front">
           <img src="${imgs[i]}.jpg">
           </div>
@@ -35,9 +35,9 @@ for (let i = 0; i < imgs.length; i++) {
           <img src="back2.jpg">
           </div>
           </div>`
-  )
+    );
+  }
 }
-};
 function shuffle(array) {
   return array.sort(() => {
     return 0.5 - Math.random();
@@ -45,29 +45,43 @@ function shuffle(array) {
 }
 var current = null;
 function clickCard() {
-    $(".card").click(function() {
-        // console.log($(this));
-        $(this)[0].children[0].style.transform = "rotateY(0deg)"
-        $(this)[0].children[1].style.transform = "rotateY(180deg)"
-        if (!current) { //check if current = null // !null == true
-          current = $(this);
-        }
-        else {
-          if (current.attr('data-name') != $(this).attr('data-name')) {
-            $(this)[0].children[0].style.transform = "rotateY(180deg)"
-            $(this)[0].children[1].style.transform = "rotateY(0deg)"
-            console.log('Different!');
-          }
-          else {
-            $(this)[0].css('display','none');
-            console.log('The same~')
-          }
-        }
+  $(".card").click(function() {
+    // console.log($(this));
+    $(this)
+      .find(".front")
+      .addClass("flip-front");
+    $(this)
+      .find(".back")
+      .addClass("flip-back");
+    $(this).css('pointer-events', 'none');
+    if (!current) {
+      //check if current = null // !null == true
+      current = $(this);
+    } else {
+      if (current.attr("data-name") != $(this).attr("data-name")) {
+        setTimeout(() => {
+          current.find('.front').toggleClass("flip-front");
+          current.find('.back').toggleClass("flip-back");
+          $(this).find('.front').toggleClass("flip-front");
+          $(this).find('.back').toggleClass("flip-back");
+          $('.card').css('pointer-events', 'auto');
+          document.getElementById('wrong-sound').play();
+          current = null;
+        }, 400);
+        // console.log("Different!");
+      } else {
+        setTimeout(() => {
+          current.css('opacity','0');
+          $(this).css('opacity','0');
+        }, 300);
+        document.getElementById('correct-sound').play();
+        // console.log("The same~");    
+      }
     }
-)
+  });
 }
 $(function() {
-shuffle(imgs);
-loadCard();
-clickCard();
-})
+  shuffle(imgs);
+  loadCard();
+  clickCard();
+});
