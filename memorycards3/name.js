ee2eb8277a60e2841c8e3481dade03da3b6b077e
huongnguyen2152360 <huongnguyen2152360@gmail.com>
2018-07-17 16:44:14 +0700

    push

--- memorycards3/name.js
+++ memorycards3/name.js
@@ -40,12 +40,14 @@ function loadCard() {
   var remainingTime = 60;
   var run = setInterval(function() {
     remainingTime--;
-    console.log(remainingTime);
+    $("progress")[0].value = remainingTime;
     if (remainingTime == 0) {
       clearInterval(run);
-      console.log("You lost");
+      $(".progress-bar").addClass("vanish");
+      $(".content").addClass("vanish");
+      $(".lose-content").addClass("appear");
+      $(".playagainBtn").addClass("appear");
     }
-    
   }, 1000);
 }
 function shuffle(array) {
@@ -55,21 +57,29 @@ function shuffle(array) {
 }
 var current = null;
 var count = 0;
+var countFlip = 0;
 function clickCard() {
   $(".card").click(function() {
-    // console.log($(this));
     $(this)
       .find(".front")
       .addClass("flip-front");
     $(this)
       .find(".back")
       .addClass("flip-back");
+    countFlip++;
     $(this).css("pointer-events", "none");
+    // console.log(countFlip);
+    // console.log("object");
+    if (countFlip == 2) {
+      $(".card").css("pointer-events", "none");
+      countFlip = 0;
+    }
     if (!current) {
       //check if current = null // !null == true
       current = $(this);
     } else {
       if (current.attr("data-name") != $(this).attr("data-name")) {
+        // console.log("object2");
         setTimeout(() => {
           current.find(".front").toggleClass("flip-front");
           current.find(".back").toggleClass("flip-back");
@@ -82,19 +92,38 @@ function clickCard() {
           $(".card").css("pointer-events", "auto");
           document.getElementById("wrong-sound").play();
           current = null;
-        }, 400);
+        }, 500);
       } else {
+        // console.log("object3");
         setTimeout(() => {
           current.css("opacity", "0");
           $(this).css("opacity", "0");
-        }, 300);
+          current = null;
+        }, 200);
+        $(".card").css("pointer-events", "auto");
         document.getElementById("correct-sound").play();
         count++;
+        if (count == 12) {
+          $(".progress-bar").addClass("vanish");
+          $(".content").addClass("vanish");
+          $(".win-content").addClass("appear");
+          $(".playagainBtn").addClass("appear");
+        }
       }
     }
   });
 }
-
+$(".playagainBtn").click(function() {
+  location.reload();
+});
+function hack() {
+  $(".card")
+    .find(".front")
+    .addClass("flip-front");
+  $(".card")
+    .find(".back")
+    .addClass("flip-back");
+}
 $(function() {
   shuffle(imgs);
   loadCard();
