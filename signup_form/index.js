0933952db27bafe3ca0238736bf85aa7c67304f6
huongnguyen2152360 <huongnguyen2152360@gmail.com>
2018-07-20 10:20:19 +0700

    push

--- signup_form/index.js
+++ signup_form/index.js
@@ -1,12 +1,18 @@
 var aString = $('.info p').html(location.href)[0].innerText;
 var quote = aString.substr(aString.search('html?')+5,aString.length);
 // username=asf&dob=aw&gender=male&address=awruy&pnumber=aery&email=ertu&password=ytq
-var quote_split = quote.split(/[=&]/);
+var deCode = decodeURIComponent(quote);
+console.log(deCode);
+var quote_split = deCode.split(/[=&]/);
 // console.log(quote_split);
 var labels = ['username','dob','gender','address','pnumber','email','password'];
 for (var i = 0; i < labels.length; i++) {
     if (quote_split.indexOf(labels[i]) != -1) {
         // console.log(quote_split.indexOf(labels[i]));
+        for (var j = 0; j < quote_split.length; j++) {
+            quote_split[j] = quote_split[j].replace(/\+/g,' ');
+            // console.log(quote_split[j]);
+            }
+        }
         document.write('Your ' + labels[i] + ': ' + quote_split[quote_split.indexOf(labels[i])+1] + '</br>');
     }
-}
