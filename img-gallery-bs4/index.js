d6107b9719078b951f1b6a546cbfe8d5f96a046c
huongnguyen2152360 <huongnguyen2152360@gmail.com>
2018-07-27 15:30:58 +0700

    push

--- img-gallery-bs4/index.js
+++ img-gallery-bs4/index.js
@@ -1,23 +1,10 @@
-$(".menu-title-icon").click(function() {
-  $(".menu-lv1").toggle();
-  if ($(".menu-lv1").css("display") == "block") {
-    $(".menu-title-icon-icon").css("display", "none");
-    $(".menu-title-icon").html(`<span class="menu-title-icon-x"><i class="fas fa-times"></i></span>`);
-  } else {
-    $(".menu-title-icon-x").css("display", "none");
-    $(".menu-title-icon").html(`<span class="menu-title-icon-icon"></span>
-    <span class="menu-title-icon-icon"></span>
-    <span class="menu-title-icon-icon"></span>`);
-  }
+$(function() {
+  $('.menu-title-icon').click(function() {
+    $('#navbar-hamburger').toggleClass('hidden');
+    $('#navbar-close').toggleClass('hidden');
+  $('.menu-lv1-item-icon-div').click(function() {
+    $('#navbar-plus').toggleClass('hidden');
+    $('#navbar-minus').toggleClass('hidden');
+  })
+  });
 });
-$('.menu-lv1-item-icon-div').click(function(){
-    $(".menu-lv2").toggle();
-    if ($(".menu-lv2").css("display") == "block") {
-        $(".menu-lv1-item-icon").css("display", "none");
-        $('.menu-lv1-item-icon-div').html(`<span class="menu-lv1-item-icon-minus">-</span>`);
-    }
-    else {
-        $('.menu-lv1-item-icon-minus').css("display", "none");
-        $('.menu-lv1-item-icon-div').html(`<span class="menu-lv1-item-icon">+</span>`)
-    }
-})
